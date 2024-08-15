dcc669f9c37b61269ec3b4381618831b5a3e5c57
federico <73360688+fedevelez0@users.noreply.github.com>
2024-08-15 07:17:07 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -5,3 +5,5 @@ st.title("Mi Primera App!!")
 st.header("en esta app desarrollare mis aplicaciomes para interfaces mutimodales")
 st.write("realizo tanto backend como frontend")
 image = Image.open('marco carola.png')
+
+st.image(image, caption= 'marco carola')
